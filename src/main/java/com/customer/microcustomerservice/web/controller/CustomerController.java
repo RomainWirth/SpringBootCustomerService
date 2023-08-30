382320b1e7439bcef05d85192050f2c1d20966c5
Romain Wirth <wirth.romain@gmail.com>
2023-08-30 09:55:24 +0200

    UPD project : ajout commentaires controllers

--- src/main/java/com/customer/microcustomerservice/web/controller/CustomerController.java
+++ src/main/java/com/customer/microcustomerservice/web/controller/CustomerController.java
@@ -10,29 +10,28 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-// arraylist avec 3 cutsomers random
 @RestController
 public class CustomerController {
-
+    // Création d'une liste de Customers
     private ArrayList<Customer> customerList = new ArrayList<>(List.of(
             new Customer(1, "D'Artagnan", "Charles", new Date(), "13AA00001"),
             new Customer(2, "de Sillègue d'Athos d'Autevielle", "Armand", new Date(), "14AA00001"),
             new Customer(3, "de Portos", "Isaac", new Date(), "15AA00001"),
             new Customer(4, "d'Aramis", "Henri", new Date(), "16AA00001")
     ));
-
+    // à l'URL "customers", on montre la liste complète des clients
     @GetMapping("/customers")
     public ArrayList<Customer> getCustomersList() {
         return customerList;
     }
-
+    // à l'URL customers/{id} (ou id est un des numéros d'id de client de la liste) on montre le client correspondant
     @GetMapping("/customers/{id}")
     public Customer displayOneCustomer(@PathVariable int id) {
-        for (Customer customer : customerList) {
-            if (id == customer.getId()) {
-                return customer;
+        for (Customer customer : customerList) { // boucle for sur le arraylist customerList
+            if (id == customer.getId()) { // comparaison de l'id passée dans l'URL avec l'id de chaque client de customerList
+                return customer; // renvoie le customer correspondant
             }
         }
-        return null;
+        return null; // sinon ne renvoie rien
     }
 }
