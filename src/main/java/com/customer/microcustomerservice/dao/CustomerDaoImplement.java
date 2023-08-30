9cf192a66d950130ec24e71097e19d0b2e70b83b
Romain Wirth <wirth.romain@gmail.com>
2023-08-30 16:38:34 +0200

    ADD méthode POST au controller + ADD Swagger pour la doc

--- src/main/java/com/customer/microcustomerservice/dao/CustomerDaoImplement.java
+++ src/main/java/com/customer/microcustomerservice/dao/CustomerDaoImplement.java
@@ -3,18 +3,31 @@ package com.customer.microcustomerservice.dao;
 import com.customer.microcustomerservice.model.Customer;
 import org.springframework.stereotype.Repository;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Repository
 public class CustomerDaoImplement implements CustomerDao {
-    public static ArrayList<Customer> customerList = new ArrayList<>(List.of(
-            new Customer(1, "D'Artagnan", "Charles", new Date(), "13AA00001"),
-            new Customer(2, "de Sillègue d'Athos d'Autevielle", "Armand", new Date(), "14AA00001"),
-            new Customer(3, "de Portos", "Isaac", new Date(), "15AA00001"),
-            new Customer(4, "d'Aramis", "Henri", new Date(), "16AA00001")
-    ));
+    public static String pattern = "yyyy-MM-dd XXX";
+    public static SimpleDateFormat sdfConvervet = new SimpleDateFormat(pattern);
+
+    public static ArrayList<Customer> customerList;
+
+    static {
+        try {
+            customerList = new ArrayList<>(List.of(
+                    new Customer(1, "D'Artagnan", "Charles", sdfConvervet.parse("1613-10-12 +00:00"), "13AA00001"),
+                    new Customer(2, "de Sillègue d'Athos d'Autevielle", "Armand", sdfConvervet.parse("1615-02-15 +00:00"), "14AA00001"),
+                    new Customer(3, "de Portos", "Isaac", sdfConvervet.parse("1617-02-02 +00:00"), "15AA00001"),
+                    new Customer(4, "d'Aramis", "Henri", sdfConvervet.parse("1620-05-28 +00:00"), "16AA00001")
+            ));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public ArrayList<Customer> findAll() {
--- src/main/java/com/customer/microcustomerservice/web/controller/CustomerController.java
+++ src/main/java/com/customer/microcustomerservice/web/controller/CustomerController.java
@@ -3,45 +3,40 @@ package com.customer.microcustomerservice.web.controller;
 
 import com.customer.microcustomerservice.dao.CustomerDao;
 import com.customer.microcustomerservice.model.Customer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Api("API pour les opérations CRUD sur les clients.")
 @RestController
 public class CustomerController {
-    // Création d'une liste de Customers
-//    private ArrayList<Customer> customerList = new ArrayList<>(List.of(
-//            new Customer(1, "D'Artagnan", "Charles", new Date(), "13AA00001"),
-//            new Customer(2, "de Sillègue d'Athos d'Autevielle", "Armand", new Date(), "14AA00001"),
-//            new Customer(3, "de Portos", "Isaac", new Date(), "15AA00001"),
-//            new Customer(4, "d'Aramis", "Henri", new Date(), "16AA00001")
-//    ));
     private final CustomerDao customerDao;
 
     public CustomerController(CustomerDao customerDao) {
         this.customerDao = customerDao;
     }
 
-
     // à l'URL "customers", on montre la liste complète des clients
+    @ApiOperation("Récupère la liste complète des clients")
     @GetMapping("/customers")
     public ArrayList<Customer> getCustomersList() {
-//        return customerList;
         return customerDao.findAll();
     }
     // à l'URL customers/{id} (ou id est un des numéros d'id de client de la liste) on montre le client correspondant
+    @ApiOperation("Récupère un client grâce à son ID à condition que celui-ci soit enregistré")
     @GetMapping("/customers/{id}")
     public Customer displayOneCustomer(@PathVariable int id) {
-//        for (Customer customer : customerList) { // boucle for sur le arraylist customerList
-//            if (id == customer.getId()) { // comparaison de l'id passée dans l'URL avec l'id de chaque client de customerList
-//                return customer; // renvoie le customer correspondant
-//            }
-//        }
-//        return null; // sinon ne renvoie rien
         return customerDao.findById(id);
     }
+
+    @ApiOperation("Méthode qui permet d'ajouter un client")
+    @PostMapping("/customers")
+    public void addCustomer(@RequestBody Customer customer) {
+        customerDao.save(customer);
+    }
+
 }
