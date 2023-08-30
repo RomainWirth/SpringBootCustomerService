0353fa58cc38397fe33576bdc8e9e28af309ac2c
Romain Wirth <wirth.romain@gmail.com>
2023-08-30 09:51:22 +0200

    UPD customer service project : arraylist

--- src/main/java/com/customer/microcustomerservice/model/Customer.java
+++ src/main/java/com/customer/microcustomerservice/model/Customer.java
@@ -1,8 +1,73 @@
 package com.customer.microcustomerservice.model;
 
+import java.util.Date;
+
 public class Customer {
     private int id;
     private String lastName;
     private String firstName;
+    private Date birthDate;
+    private String drivingLicence;
 
+    public Customer() {
+    }
+
+    public Customer(int id, String lastName, String firstName, Date birthDate, String drivingLicence) {
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.birthDate = birthDate;
+        this.drivingLicence = drivingLicence;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getDrivingLicence() {
+        return drivingLicence;
+    }
+
+    public void setDrivingLicence(String drivingLicence) {
+        this.drivingLicence = drivingLicence;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", birthDate=" + birthDate +
+                ", drivingLicence='" + drivingLicence + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/customer/microcustomerservice/web/controller/CustomerController.java
+++ src/main/java/com/customer/microcustomerservice/web/controller/CustomerController.java
@@ -1,18 +1,38 @@
 package com.customer.microcustomerservice.web.controller;
 
 
+import com.customer.microcustomerservice.model.Customer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+// arraylist avec 3 cutsomers random
 @RestController
 public class CustomerController {
+
+    private ArrayList<Customer> customerList = new ArrayList<>(List.of(
+            new Customer(1, "D'Artagnan", "Charles", new Date(), "13AA00001"),
+            new Customer(2, "de Sillègue d'Athos d'Autevielle", "Armand", new Date(), "14AA00001"),
+            new Customer(3, "de Portos", "Isaac", new Date(), "15AA00001"),
+            new Customer(4, "d'Aramis", "Henri", new Date(), "16AA00001")
+    ));
+
     @GetMapping("/customers")
-    public String customersList() {
-        return "customer example";
+    public ArrayList<Customer> getCustomersList() {
+        return customerList;
     }
+
     @GetMapping("/customers/{id}")
-    public String displayOneCustomer(@PathVariable int id) {
-        return "You asked for customer n° : " + id;
+    public Customer displayOneCustomer(@PathVariable int id) {
+        for (Customer customer : customerList) {
+            if (id == customer.getId()) {
+                return customer;
+            }
+        }
+        return null;
     }
 }
